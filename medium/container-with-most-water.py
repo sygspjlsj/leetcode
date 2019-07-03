095a13480a88bdeb97ff11a171a42034bd7bf94d
光龙 <yueguang.syg@koubei.com>
2019-07-03 09:30:45 +0800

    container-with-most-water.py

---
+++ medium/container-with-most-water.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        maxarea = 0
+        l, r = 0, len(height) - 1
+        while l < r:
+            if height[l] < height[r]:
+                maxarea = max(maxarea, height[l] * (r - l))
+                l += 1
+            else:
+                maxarea = max(maxarea, height[r] * (r - l))
+                r -= 1
+        return maxarea
